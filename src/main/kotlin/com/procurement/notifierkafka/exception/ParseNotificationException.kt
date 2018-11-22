446e216f8b0d3c7c92161070153834c759370e6e
Maxim Sambulat <maxim.sambulat@gmail.com>
2018-11-22 12:57:41 +0200

    Fixed problem of processing the message with incorrect format

--- src/main/kotlin/com/procurement/notifierkafka/exception/ParseNotificationException.kt
+++
@@ -1,4 +0,0 @@
-package com.procurement.notifierkafka.exception
-
-class ParseNotificationException(notification: String) :
-    RuntimeException("Error of convert 'JSON' to request body: '$notification'")
